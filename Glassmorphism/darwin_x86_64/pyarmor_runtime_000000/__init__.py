f20b7e8fd7726aa5a9d9320449ca5693050d260e
GvozdevLeonid <gvozdev-leonid-95@yandex.ru>
2023-04-23 21:15:47 +0200

    fixed import errors

---
+++ Glassmorphism/darwin_x86_64/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.1.7 (trial), 000000, 2023-04-23T20:53:12.066621
+from .pyarmor_runtime import __pyarmor__
--- Glassmorphism/linux.x86/pyarmor_runtime_000000/__init__.py
+++
@@ -1,2 +0,0 @@
-# Pyarmor 8.1.7 (trial), 000000, 2023-04-23T20:22:11.804584
-from .pyarmor_runtime import __pyarmor__
